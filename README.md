dc18d3acc56bd4bed2703647283b9e60c217fd28
nulltmntr <git.cybergod.repo@gmail.com>
2023-01-29 11:21:59 -0500

    added a readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Pand0raOS
+
+A simple operating system in assembly & C.
+
+Follow me on Twitter @[nulltmntr](twitter.com/nulltmntr)
+
+> Special thanks to:
+- nanobyte
+- ios_euphoria
--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -1,13 +1,30 @@
 org 0h7c00
 bits 16
 
-main:            
-    hlt
+print:
+    push bx
 
+.repeat:
+    mov al, [bx]
+    cmp al, 0
+    je .done
+    mov ah, 0h0E
+    int 0h10
+    add bx, 1
+    jmp .repeat
 
+.done:
+    pop bx
+    ret
+
+main:
+    mov bx, OS_BOOT_MSG
+    call print
 
 .halt:
     jmp .halt
-    
+
+OS_BOOT_MSG: db "[*] Booting Pand0raOS ...", 0
+
 times 510-($-$$) db 0
 dw 0hAA55
