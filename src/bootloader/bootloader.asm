d6272b92a4e7fd9d131299adf52fb103f6a57c44
nulltmntr <git.cybergod.repo@gmail.com>
2023-01-29 21:53:56 -0500

    [*] Added FAT 12 headers to fix broken file system on floppy image

--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -1,8 +1,32 @@
 org 0h7c00
 bits 16
 
-start:
-    jmp main
+; FAT 12 headers
+jmp short main
+nop
+
+; => bios paramater block
+db 'MSWIN4.1'               ; oem_identifier
+dw  512                     ; bytes per sector
+db  1                       ; sector per cluster
+dw  1                       ; reserved sectors
+db  2                       ; fat count
+dw  0E0h                    ; dir entries count
+dw  2880                    ; total sectors
+db  0F0h                    ; media descriptor
+dw  9                       ; sectors per fat
+dw  18                      ; sectors per track
+dw  2                       ; heads
+dd  0                       ; hidden sectors
+dd  0                       ; large sector count
+
+; => extended boot record
+db  0                       ; drive number
+db  0
+db  0h29                    ; signature
+db  0h13, 0h43, 0h74, 0h21  ; volume id
+db  'Pand0raOS  '           ; volume label
+db  'FAT12   '              ; system label
 
 print:
     pusha
